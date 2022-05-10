d9c4c9a694248c934ae8f950d331d6e3dbc633ae
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-05-10 09:20:19 +0500

    Update Topics.jsx

--- frontend/src/components/Docs/Topics.jsx
+++ frontend/src/components/Docs/Topics.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Accordion } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios';
+import { apiip } from '../../serverConfig'
 export default function Topics() {
+    const [Docs, setDocs] = useState([])
+    useEffect(() => {
+        var config = {
+            method: 'get',
+            url: `${apiip}/docs/api/docs`,
+            headers: {}
+        };
+
+        axios(config)
+            .then(function (response) {
+                if (response.data.status === "ok") {
+                    console.log(response.data.data);
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    }, [axios])
+
     return (
         <div className='topics'>
-
+            <h1>Welcome to Docs</h1>
             <Accordion defaultActiveKey="0">
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Accordion Item #1</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
-                    <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
