ae03adcea99ed5db5208384e82f5bd6696abaed7
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-05-10 09:02:15 +0500

    Update Topics.jsx

--- frontend/src/components/Docs/Topics.jsx
+++ frontend/src/components/Docs/Topics.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Accordion } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function Topics() {
     return (
         <div className='topics'>
-            <ul>
+
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+
+
+            {/* <ul>
 
 
                 <li data-nav-id="/products/platform/" class="dd-item">
@@ -322,7 +352,7 @@ export default function Topics() {
                         </li>
                     </ul>
                 </li>
-            </ul>
+            </ul> */}
         </div>
     )
 }
