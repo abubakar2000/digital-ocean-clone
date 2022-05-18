f0515bf5593bd2a6538a3d0922a80d3c7852a437
WaliAmed <waliamedvd@gmail.com>
2022-05-18 17:12:38 +0500

    css fixes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import bg from "../img/bg.jpg";
 import { Link } from "react-router-dom";
 import GoogleLogin from "react-google-login";
 import { google_clientId } from "../serverConfig";
+import "./style.css";
 
 export default function HomePage() {
   const [isLoggedIn, setIsLoggedIn] = useState("");
@@ -24,100 +25,97 @@ export default function HomePage() {
         <Navbar></Navbar>
 
         {isLoggedIn !== "true" && (
-          <div>
+          <div
+            className="home-page row front-bg text-light"
+            style={{
+              backgroundImage: `url(${bg})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              marginTop: "60pt",
+            }}
+          >
             <div
-              className="row front-bg text-light"
-              style={{
-                backgroundImage: `url(${bg})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                marginTop: "60pt",
-                width: "100%",
-              }}
+              className="col-1"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            ></div>
+            <div
+              className="col-10 text-center py-5"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
             >
-              <div
-                className="col-1"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              ></div>
-              <div
-                className="col-10 text-center py-5"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              >
-                <h1
-                  style={{
-                    fontSize: "80px",
-                    color: "white",
-                    fontWeight: "bold",
-                  }}
-                >
-                  Simple, affordable virtual machines
-                </h1>
-              </div>
-              <div
-                className="col-1"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              ></div>
-              <div
-                className="col-2"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              ></div>
-              <div
-                className="col-8"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              >
-                <p>
-                  Get dependable uptime with our 99.99% SLA, simple security
-                  tools, and predictable monthly pricing with DigitalOcean's
-                  virtual machines, called Droplets.
-                </p>
-              </div>
-              <div
-                className="col-2"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              ></div>
-              <div
-                className="col-2"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              ></div>
-              <div
-                className="text-center"
+              <h1
                 style={{
-                  backgroundColor: "rgba(0, 0, 0, 0.4)",
-                  margin: "0 auto",
+                  fontSize: "80px",
+                  color: "white",
+                  fontWeight: "bold",
                 }}
               >
-                <Link to={"/login"}>
-                  <button className="btn btn-primary rounded-1 btn-md text-ligt py-2 m-1">
-                    Signup with email
-                  </button>
-                </Link>
-
-                <GoogleLogin
-                  clientId={google_clientId}
-                  buttonText="Sign In with Gmail"
-                  onSuccess={responseGoogle}
-                  onFailure={responseGoogle}
-                  cookiePolicy={"single_host_origin"}
-                />
-
-                <button className="btn btn-light rounded-3 btn-md text-ligt py-2 m-1">
-                  <img
-                    src="https://www-static.cdn.prismic.io/www-static/0a3e37e0-1706-41d5-98d1-854585205a5e_github-logo.svg"
-                    alt=""
-                    width="20"
-                  />{" "}
-                  Sign In with Github
-                </button>
-              </div>
-              <div
-                className="col-2"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              ></div>
-              <div
-                className="col-12 py-5"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-              ></div>
+                Simple, affordable virtual machines
+              </h1>
             </div>
+            <div
+              className="col-1"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            ></div>
+            <div
+              className="col-2"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            ></div>
+            <div
+              className="col-8"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            >
+              <p>
+                Get dependable uptime with our 99.99% SLA, simple security
+                tools, and predictable monthly pricing with DigitalOcean's
+                virtual machines, called Droplets.
+              </p>
+            </div>
+            <div
+              className="col-2"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            ></div>
+            <div
+              className="col-2"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            ></div>
+            <div
+              className="text-center"
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.4)",
+                margin: "0 auto",
+              }}
+            >
+              <Link to={"/login"}>
+                <button className="btn btn-primary rounded-1 btn-md text-ligt py-2 m-1">
+                  Signup with email
+                </button>
+              </Link>
+
+              <GoogleLogin
+                clientId={google_clientId}
+                buttonText="Sign In with Gmail"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={"single_host_origin"}
+              />
+
+              <button className="btn btn-light rounded-3 btn-md text-ligt py-2 m-1">
+                <img
+                  src="https://www-static.cdn.prismic.io/www-static/0a3e37e0-1706-41d5-98d1-854585205a5e_github-logo.svg"
+                  alt=""
+                  width="20"
+                />{" "}
+                Sign In with Github
+              </button>
+            </div>
+            <div
+              className="col-2"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            ></div>
+            <div
+              className="col-12 py-5"
+              style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
+            ></div>
           </div>
         )}
 
